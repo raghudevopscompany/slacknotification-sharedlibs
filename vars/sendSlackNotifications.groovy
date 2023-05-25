a950f3cc923912578f907efda5d6fdba57aec7cb
raghu2671 <125441353+raghu2671@users.noreply.github.com>
2023-05-25 23:50:13 +0530

    Update sendSlackNotifications.groovy
    rollbacking of my previous commit

--- vars/sendSlackNotifications.groovy
+++ vars/sendSlackNotifications.groovy
@@ -1,4 +1,4 @@
-def SendSlackNotifications(String buildStatus = 'STARTED') {
+def call(String buildStatus = 'STARTED') {
   // build status of null means successful
   //This is the condition which we are checking weather buildStatus is SUCCESSFULL or not.
  //This line updated to show the Eclipse with GitHub demo
